904f85066036ac2e81b9fee3dd4ffb4a2ac4ac70
sarahwiley <sarah@mainframe.com>
2018-10-25 14:33:59 -0600

    code cleanup. add react-native-web.

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native-web'
+
+const styles = StyleSheet.create({
+  root: {
+    textAlign: 'center'
+  },
+  largeText: {
+    fontSize: '40px',
+    fontWeight: 'bold'
+  }
+})
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.root}>
+        <Text style={styles.largeText}>Welcome to Noted.<br/>Please take some notes.</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
